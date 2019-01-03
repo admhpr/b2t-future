29d1d710aea6df8aa4f44559452bd48c24c4d999
Adam Harpur <adam@harpur.io>
2019-01-03 11:29:37 -0600

    added anime.js

---
+++ src/js/anime.js
@@ -0,0 +1,16 @@
+// Wrap every letter in a span
+$('.tag-line .letters').each(function () {
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+anime.timeline({
+        loop: false
+    })
+    .add({
+        targets: '.tag-line .letter',
+        scale: [0, 1],
+        duration: 3000,
+        elasticity: 600,
+        delay: function (el, i) {
+            return 45 * (i + 1)
+        }
+    })
