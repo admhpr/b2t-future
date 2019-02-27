b878e02635b5ea492dd20bb7e5dcf8a856bf6b8c
Adam Harpur <adam@harpur.io>
2019-02-27 11:39:27 -0600

    chore: speed up animation

--- src/js/anime.js
+++ src/js/anime.js
@@ -1,12 +1,16 @@
 // Wrap every letter in a span
-$('.tag-line .letters').each(function () {
-    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+$(".tag-line .letters").each(function() {
+  $(this).html(
+    $(this)
+      .text()
+      .replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
+  );
 });
 
 anime({
-    targets: '.text-wrapper .letter',
-    translateX: [-1000, 0],
-    direction: 'linear',
-    duration: 2000,
-    delay: anime.stagger(100)
-})
+  targets: ".text-wrapper .letter",
+  translateX: [-1000, 0],
+  direction: "linear",
+  duration: 1000,
+  delay: anime.stagger(100)
+});
