2deb4608697a40d4cfc3ff3e4a67a985944ee0c0
Adam Harpur <adam@harpur.io>
2019-02-27 11:23:15 -0600

    feat: added new animation for tagline

--- src/js/anime.js
+++ src/js/anime.js
@@ -3,15 +3,10 @@ $('.tag-line .letters').each(function () {
     $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
 
-anime.timeline({
-        loop: false
-    })
-    .add({
-        targets: '.tag-line .letter',
-        scale: [0, 1],
-        duration: 3000,
-        elasticity: 2000,
-        delay: function (el, i) {
-            return 45 * (i + 1)
-        }
-    })
+anime({
+    targets: '.text-wrapper .letter',
+    translateX: [-1000, 0],
+    direction: 'linear',
+    duration: 2000,
+    delay: anime.stagger(100)
+})
